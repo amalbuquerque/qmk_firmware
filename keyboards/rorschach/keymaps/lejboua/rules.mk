727e59a4b38e5d259a7cba2223d6820027a7d813
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2018-09-16 21:49:40 +0100

    Rorschach lejboua keymap adapted from Nyquist lejboua
    Not using tap dance yet.

--- keyboards/rorschach/keymaps/lejboua/rules.mk
+++ keyboards/rorschach/keymaps/lejboua/rules.mk
@@ -16,6 +16,6 @@ UNICODE_ENABLE = yes        # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
-TAP_DANCE_ENABLE = yes      # Enable Tap Dancing function
+TAP_DANCE_ENABLE = no       # Enable Tap Dancing function
 
 

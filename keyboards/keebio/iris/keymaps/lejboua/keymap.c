5f397ec9e6d2efed851c6c9b86abd4827eedaf05
André Albuquerque <557562+amalbuquerque@users.noreply.github.com>
2022-02-13 21:17:01 +0000

    Replace MACRODOWN by SEND_STRING + fix compilation errors after rebase

--- keyboards/keebio/iris/keymaps/lejboua/keymap.c
+++ keyboards/keebio/iris/keymaps/lejboua/keymap.c
@@ -27,27 +27,25 @@ enum iris_keycodes {
   TTO_FN1 = TT(_F1_LAYER),
   TTO_TMX = TT(_TMUX_LAYER),
   NKROTG = MAGIC_TOGGLE_NKRO,
+
+  // Macros
+  PARENS,
+  CRLY_PARENS,
+  SQUR_PARENS,
+  TMUX_WIN1,
+  TMUX_WIN2,
+  TMUX_WIN3,
+  TMUX_WIN4,
+  TMUX_WIN5,
+  TMUX_ZOOM,
+  TMUX_LWIN,
+  TMUX_RWIN,
+  TMUX_HSPL,
+  TMUX_VSPL
 };
 
 #define ______ KC_TRNS
-#define _______ KC_TRNS
 #define XXXXXXX KC_NO
-
-enum macro_id {
-    PARENS,
-    CRLY_PARENS,
-    SQUR_PARENS,
-    TMUX_WIN1,
-    TMUX_WIN2,
-    TMUX_WIN3,
-    TMUX_WIN4,
-    TMUX_WIN5,
-    TMUX_ZOOM,
-    TMUX_LWIN,
-    TMUX_RWIN,
-    TMUX_HSPL,
-    TMUX_VSPL
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty (uses Space Cadet Shifts) */
@@ -98,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬───────────┬───────────┬─────────┐                          ┌────────┬──────────┬───────────┬───────────┬──────────┬────────┐
      ______,  S(KC_1), S(KC_2), S(KC_3),    S(KC_4),    S(KC_5),                             S(KC_6), S(KC_7),   S(KC_8),    S(KC_9),    S(KC_0),   ______,
   //├────────┼────────┼────────┼───────────┼───────────┼─────────┤                          ├────────┼──────────┼───────────┼───────────┼──────────┼────────┤
-     ______,  S(KC_9), S(KC_0), S(KC_LBRC), S(KC_RBRC), M(0),                                M(1),    KC_QUOT,   S(KC_QUOT), S(KC_MINS), S(KC_EQL), ______,
+     ______,  S(KC_9), S(KC_0), S(KC_LBRC), S(KC_RBRC), PARENS,                                CRLY_PARENS,    KC_QUOT,   S(KC_QUOT), S(KC_MINS), S(KC_EQL), ______,
   //├────────┼────────┼────────┼───────────┼───────────┼─────────┤                          ├────────┼──────────┼───────────┼───────────┼──────────┼────────┤
-     ______,  KC_LBRC, KC_RBRC, ______,     ______,     M(2),                                ______,  ______,    ______,     ______,     ______,    ______,
+     ______,  KC_LBRC, KC_RBRC, ______,     ______,     SQUR_PARENS,                                ______,  ______,    ______,     ______,     ______,    ______,
   //├────────┼────────┼────────┼───────────┼───────────┼─────────┼────────┐        ┌────────┼────────┼──────────┼───────────┼───────────┼──────────┼────────┤
      ______,  ______,  ______,  ______,     ______,     ______,   ______,           ______,  ______,  ______,    ______,     ______,     ______,    ______,
   //└────────┴────────┴────────┴───┬───────┴───┬───────┴───┬─────┴───┬────┘        └───┬────┴───┬────┴──────┬───┴─────┬─────┴───────────┴──────────┴────────┘
@@ -130,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼─────────┼───────────┼───────────┼─────────┤                          ├─────────┼──────────┼───────────┼───────────┼──────────┼────────┤
      ______,  KC_VOLU,  KC_VOLD,  KC_MUTE,    KC_MPLY,    ______,                              RGB_M_P,  RGB_M_B,   RGB_M_R,    RGB_M_K,    RGB_M_SW, ______,
   //├────────┼────────┼─────────┼───────────┼───────────┼─────────┼────────┐        ┌────────┼─────────┼──────────┼───────────┼───────────┼──────────┼────────┤
-     ______,  ______,  ______,   ______,     ______,     ______,   ______,           RGB_TOG,  ______,   ______,    ______,     ______,     ______,   ______,
+     ______,  ______,  ______,   ______,     ______,     ______,   RGB_MOD,          RGB_TOG,  ______,   ______,    ______,     ______,     ______,   ______,
   //└────────┴────────┴─────────┴───┬───────┴───┬───────┴───┬─────┴───┬────┘        └───┬────┴───┬─────┴──────┬───┴─────┬─────┴───────────┴──────────┴────────┘
                                       ______,     ______,    ______,                     ______,   ______,     ______
                                 //  └───────────┴───────────┴─────────┘                 └────────┴───────────┴─────────┘
@@ -140,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬─────────┬───────────┬──────────┬────────────┐                          ┌────────┬──────────┬───────────┬───────────┬──────────┬────────┐
      ______,  ______,   ______,   ______,      ______,   ______,                                 ______,  ______,    ______,      ______,    ______,    ______,
   //├────────┼────────┼─────────┼───────────┼──────────┼────────────┤                          ├────────┼──────────┼───────────┼───────────┼──────────┼────────┤
-     ______,  M(3),     M(4),     M(5),       M(6),      M(7),                                   M(8),    M(9),      M(10),       M(11),     M(12),     ______,
+     ______,  TMUX_WIN1,  TMUX_WIN2,  TMUX_WIN3,       TMUX_WIN4,  TMUX_WIN5,                                   TMUX_ZOOM,  TMUX_LWIN,  TMUX_RWIN,  TMUX_HSPL,  TMUX_VSPL,     ______,
   //├────────┼────────┼─────────┼───────────┼──────────┼────────────┤                          ├────────┼──────────┼───────────┼───────────┼──────────┼────────┤
      ______,  ______,   ______,   ______,     ______,    ______,                                 ______,  ______,    ______,     ______,     ______,    ______,
   //├────────┼────────┼─────────┼───────────┼──────────┼────────────┼────────┐        ┌────────┼────────┼──────────┼───────────┼───────────┼──────────┼────────┤
@@ -184,79 +182,6 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 #endif  // ENCODER_ENABLE
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-    // MACRODOWN only works in this function
-    switch(id) {
-        case PARENS: // Macro ()
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LSFT), TYPE(KC_9), TYPE(KC_0), UP(KC_LSFT), TYPE(KC_LEFT), END );
-            }
-            break;
-        case CRLY_PARENS: // Macro {\n\n}
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LSFT), TYPE(KC_LBRC), TYPE(KC_RBRC), UP(KC_LSFT), TYPE(KC_LEFT), TYPE(KC_ENT), TYPE(KC_ENT), TYPE(KC_UP), END );
-            }
-            break;
-        case SQUR_PARENS: // Macro []
-            if (record->event.pressed) {
-                return MACRODOWN( TYPE(KC_LBRC), TYPE(KC_RBRC), TYPE(KC_LEFT), END );
-            }
-            break;
-        case TMUX_WIN1: // Macro Tmux Win1
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_1), END );
-            }
-            break;
-        case TMUX_WIN2: // Macro Tmux Win2
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_2), END );
-            }
-            break;
-        case TMUX_WIN3: // Macro Tmux Win3
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_3), END );
-            }
-            break;
-        case TMUX_WIN4: // Macro Tmux Win4
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_4), END );
-            }
-            break;
-        case TMUX_WIN5: // Macro Tmux Win5
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_5), END );
-            }
-            break;
-        case TMUX_ZOOM: // Macro Tmux Zoom toggle
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_Z), END );
-            }
-            break;
-        case TMUX_LWIN: // Macro Tmux Left Window
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), TYPE(KC_H), UP(KC_LCTL), END );
-            }
-            break;
-        case TMUX_RWIN: // Macro Tmux Right Window
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), TYPE(KC_L), UP(KC_LCTL), END );
-            }
-            break;
-        case TMUX_HSPL: // Macro Tmux Horizontal Split
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), DOWN(KC_RSFT), TYPE(KC_QUOT), UP(KC_RSFT), END );
-            }
-            break;
-        case TMUX_VSPL: // Macro Tmux Vertical Split
-            if (record->event.pressed) {
-                return MACRODOWN( DOWN(KC_LCTL), TYPE(KC_A), UP(KC_LCTL), TYPE(KC_SLSH), END );
-            }
-            break;
-    }
-    return MACRO_NONE;
-};
-
 /* This functions is called whenever a layer changes */
 // in our case we're calling the `update_tri_layer_state`, meaning
 // we will activate the _ADJUST layer if both NUMBERS and NUMBERS2
@@ -272,6 +197,7 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    // LAYERS
     case QWERTY:
       if (record->event.pressed) {
         persistent_default_layer_set(1UL << _QWERTY);
@@ -295,6 +221,72 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_TMUX_LAYER);
       }
       return false;
+      break;
+    // MACROS
+    case PARENS: // Macro ()
+      if (record->event.pressed) {
+          SEND_STRING(SS_LSFT("9") SS_LSFT("0") SS_TAP(X_LEFT));
+      }
+      break;
+    case CRLY_PARENS: // Macro {\n\n}
+      if (record->event.pressed) {
+          SEND_STRING(SS_LSFT("[") SS_LSFT("]") SS_TAP(X_LEFT) SS_TAP(X_ENT) SS_TAP(X_ENT) SS_TAP(X_UP));
+      }
+      break;
+    case SQUR_PARENS: // Macro []
+      if (record->event.pressed) {
+          SEND_STRING("[]" SS_TAP(X_LEFT));
+      }
+      break;
+    case TMUX_WIN1: // Macro Tmux Win1
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "1");
+      }
+      break;
+    case TMUX_WIN2: // Macro Tmux Win2
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "2");
+      }
+      break;
+    case TMUX_WIN3: // Macro Tmux Win3
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "3");
+      }
+      break;
+    case TMUX_WIN4: // Macro Tmux Win4
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "4");
+      }
+      break;
+    case TMUX_WIN5: // Macro Tmux Win5
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "5");
+      }
+      break;
+    case TMUX_ZOOM: // Macro Tmux Zoom toggle
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "z");
+      }
+      break;
+    case TMUX_LWIN: // Macro Tmux Left Window
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") SS_LCTL("h"));
+      }
+      break;
+    case TMUX_RWIN: // Macro Tmux Right Window
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") SS_LCTL("l"));
+      }
+      break;
+    case TMUX_HSPL: // Macro Tmux Horizontal Split
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") SS_LSFT("'"));
+      }
+      break;
+    case TMUX_VSPL: // Macro Tmux Vertical Split
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTL("a") "/");
+      }
       break;
   }
   return true;
